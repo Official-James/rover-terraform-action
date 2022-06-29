526d74e9435a1478ca6787460be6f118b441d568
James Cook <james.a.cook.92@gmail.com>
2022-06-29 18:57:54 +0100

    add planjsonpath and tfconfigexists

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [[ -n "$PLANPATH" ]]
 then
-    rover -standalone true -planPath $PLANPATH
+    rover -standalone true -planJSONPath $PLANPATH -tfConfigExists false
 else
     rover -standalone true
 fi
