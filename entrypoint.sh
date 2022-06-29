a263a66901b0da7be1ddf963887407ef2430db11
James Cook <james.a.cook.92@gmail.com>
2022-06-29 18:52:06 +0100

    amend to use -n instead of -z

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ -z "$PLANPATH" ]
+if [[ -n "$PLANPATH" ]]
 then
-    rover -standalone true
-else
     rover -standalone true -planPath $PLANPATH
+else
+    rover -standalone true
 fi
